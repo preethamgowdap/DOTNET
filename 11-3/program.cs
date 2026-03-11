efc080e430a4cd6fbad04078c17b81d6b34332be
Preetham gowda n <prtmpreethu@gmail.com>
2026-03-11 15:42:15 +0530

    Create program.cs

---
+++ 11-3/program.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int age = 25;                 
+        float height = 5.9f;          
+        bool isStudent = true;        
+        string name = "Preetham";     
+
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Is Student: " + isStudent);
+        Console.WriteLine("Name: " + name);
+    }
+}
