8983a2d2ce4b43b3f0899c43a5cc86852beb4959
Preetham gowda n <prtmpreethu@gmail.com>
2026-03-13 10:53:10 +0530

    Create Program.cs

---
+++ 12-03/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+       static void Main()
+    {
+       int x=10;
+       int y=7;
+       var add=x+y;
+       var sub=x-y;
+       var mul=x*y;
+       var div=(double)x/y;
+
+        Console.WriteLine("Add: " + add);
+        Console.WriteLine("Sub: " + sub);
+        Console.WriteLine("Mul: " + mul);
+        Console.WriteLine("Div: " +div);
+    }
+}
