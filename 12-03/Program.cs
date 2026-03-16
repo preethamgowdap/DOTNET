34a4dbd4f1549f5e87ffa3c3fc7162a7e6b2c45a
Preetham gowda n <prtmpreethu@gmail.com>
2026-03-16 12:35:19 +0530

    Update Program.cs

--- 12-03/Program.cs
+++ 12-03/Program.cs
@@ -1,19 +1,47 @@
 using System;
 
-class Program
+namespace MyApplication
 {
-       static void Main()
+    class Program
     {
-       int x=10;
-       int y=7;
-       var add=x+y;
-       var sub=x-y;
-       var mul=x*y;
-       var div=(double)x/y;
 
-        Console.WriteLine("Add: " + add);
-        Console.WriteLine("Sub: " + sub);
-        Console.WriteLine("Mul: " + mul);
-        Console.WriteLine("Div: " +div);
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                return 0;
+            }
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum: " + Add(x, y));
+            Console.WriteLine("Sub: " + Sub(x, y));
+            Console.WriteLine("Mul: " + Mul(x, y));
+            Console.WriteLine("Div: " + Div(x, y));
+        }
     }
 }
